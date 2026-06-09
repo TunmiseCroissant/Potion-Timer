dea61efd54b8bfff31360cbb79fdddd36fef326e
Tunmi <tunmifolorunsho@gmail.com>
2026-06-09 05:18:23 -0500

    added a calculator

--- func.js
+++ func.js
@@ -1,3 +1,21 @@
 function openDiv(id){
     document.getElementById(id).style.display = "flex";
 }
+
+const display = document.getElementById("calcDisplay");
+
+function appendToDisplay(char) {
+    display.value += char;
+}
+
+function clearCalc() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value)
+    } catch (err) {
+        display.value = error;
+    }
+}
